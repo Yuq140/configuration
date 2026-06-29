e754c47e97b2e71812b110e7c6939c0829233125
Yuq140 <alaire3@gmail.com>
2026-06-29 15:38:16 -0400

    initial astro setup

--- nvim/lua/plugins/stylua.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "ckipp01/stylua-nvim",
-    config = function()
-        local stylua = require("stylua-nvim")
-        vim.keymap.set("n", "<M-S-f>", stylua.format_file, {})
-    end,
-}
